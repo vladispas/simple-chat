9266e95265ec3df34a1069692ab7bd4fffc5783d
Vlad Ispas <vlad.ispas@yahoo.com>
2024-07-01 15:55:06 +0300

    Created Xcode project.

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Vlad Ispas on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
